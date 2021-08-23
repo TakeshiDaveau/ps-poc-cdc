e8e026d7037d3a599fe56fb10c8bff81e180a227
TakeshiDaveau <takeshi.daveau@prestashop.com>
2021-08-23 14:28:36 +0200

    Initial commit

---
+++ poc-cdc-app/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <my-hello-world></my-hello-world>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+// import HelloWorld from './components/HelloWorld.vue'
+import * as zoid from 'zoid/dist/zoid.frameworks.js'
+
+const MyHelloWorld = zoid.create({
+  tag: 'my-hellow-world',
+  url: 'http://localhost:8089',
+  dimensions: {
+    width: '500px',
+    height: '700px'
+  }
+}).driver('vue', Vue);
+
+export default {
+  name: 'App',
+  components: {
+    'my-hello-world': MyHelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ poc-cdc-component/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import * as zoid from 'zoid/dist/zoid.frameworks.js'
+
+
+zoid.create({
+  tag: 'my-hellow-world',
+  url: 'http://localhost:8089'
+});
+
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
